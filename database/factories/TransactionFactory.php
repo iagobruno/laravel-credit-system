f113a06b14b9e6cd82f8d751ec3bb3eaf99e43a6
Iago Bruno <iagobruno.dev@gmail.com>
2021-12-06 01:03:45 -0300

    feat: Adds Transaction model, migration and factory

---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TransactionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $amount = $this->faker->numberBetween(-10_000, 10_000);
+
+        return [
+            'amount' => $amount,
+            'description' => $amount >= 0 ? 'Added coins' : 'Bought an item'
+        ];
+    }
+}
